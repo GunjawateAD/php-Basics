5dd85eb40ad955cf713ed49b4852badeb914307a
Avishkar Dattatray Gunjawate <avishkargunjawate76@gmail.com>
2023-03-14 18:46:57 +0530

    initial commit

---
+++ blank.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <?php
+            $title = "Avishkar Site";
+            ?>
+
+    <h1><?php echo $title; ?></h1> -->
+
+    <?php
+    //Variables
+    // $name = 'Avishkar';
+    // $number = 100;
+    // $number_List = 100;
+    // $num-ber = 400;
+    // $0number = 500;
+    // $NUMBER = 500;
+    // $float = 100.5;
+
+    // echo $name ." " . $number;
+
+    // $name = "<h1> HELLO</h1>";
+
+    // echo $name;
+
+    //Math
+    // echo 56 + 45;
+    // echo "<br>";
+    // echo 56 - 45;
+    // echo "<br>";
+    // echo 56 * 45;
+    // echo "<br>";
+    // echo 56 / 45;
+    // echo "<br>";
+    // echo "<br>";
+
+    // echo 45 + 34 * 45 / 421 - 45;
+    // echo "<br>";
+    // echo 5 + 5 / 10;
+
+    // echo "<br>";
+    // $number1 = 12;
+    // $number2 = 24;
+
+    // echo $number1 + $number2;
+
+    //Arrays
+    // $numberList = array(267,8765,345,'5345',345,'<h1>Hello</h1>');
+    // // echo $numberList[5];
+
+    // //print_r($numberList);
+
+    // for($x=0;$x<=5;$x++)
+    // {
+    //     echo $numberList[$x] . "<br>";
+    // }
+
+    //arrays associative
+    // $number = array(10,20,49);
+    // print_r($number);
+
+    // echo $number[2] . "<br>";
+
+    // $names = ["first_name" => 'avishkar',"Last_Name" => 'gunjawate'];
+
+    // print_r($names);
+
+    // echo $names['first_name'] . " " . $names['Last_Name'];
+
+    //if else
+    // if (3 > 10) {
+    //     echo "3 is less than 10";
+    // } elseif (4 < 5) {
+    //     echo "of course 4 is less than 5";
+    // } else {
+    //     echo "it is not";
+    // }
+
+    //Comparison & Logical Operators
+    /* Comparison Operators
+        equal ==
+        identical ===
+        compare > < >= <= <>
+        not equal !=
+        not identical !==*/
+
+    /*Logical Operators 
+        And &&
+        Or ||
+        Not !*/
+
+        // if( 4 === 4 && 5 <10)
+        // {
+        //     echo "it is true";
+        // }
+    ?>
+
+
+</body>
+
+</html>
